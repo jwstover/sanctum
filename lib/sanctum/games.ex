d66aac152ceb9bb9cff4f16fe605fc67c32ffcd7
Jacob Stover <jacob.stover@ahead.com>
2025-08-26 09:22:55 -0400

    feat: show all games on home page

--- lib/sanctum/games.ex
+++ lib/sanctum/games.ex
@@ -16,6 +16,7 @@ defmodule Sanctum.Games do
     resource Sanctum.Games.Game do
       define :create_game, action: :create
       define :get_game, get_by: :id, action: :read
+      define :list_games, action: :read_games_for_user, args: [:user_id]
     end
 
     resource Sanctum.Games.Scenario do
--- lib/sanctum_web/live/game_live/index.ex
+++ lib/sanctum_web/live/game_live/index.ex
@@ -5,18 +5,55 @@ defmodule SanctumWeb.GameLive.Index do
 
   on_mount {SanctumWeb.LiveUserAuth, :live_user_optional}
 
+  alias Sanctum.Games
+
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, socket |> assign_games()}
   end
 
   def handle_event("new-game", _params, socket) do
     {:noreply, push_navigate(socket, to: ~p"/games/new")}
   end
 
+  def assign_games(socket) do
+    {:ok, games} =
+      Games.list_games(socket.assigns.current_user.id,
+        query: [sort: [inserted_at: :desc]],
+        load: [game_villian: [:card]],
+        actor: socket.assigns.current_user
+      )
+
+    assign(socket, :games, games)
+  end
+
   def render(assigns) do
     ~H"""
     <Layouts.app current_user={@current_user} flash={@flash}>
       <.button :if={@current_user} variant="primary" phx-click="new-game">New Game</.button>
+
+      <div class="flex flex-col gap-2 mt-2 font-elektra">
+        <div
+          :for={game <- @games}
+          class="px-4 py-2 border-b-4 border-t-2 border-l-1 border-r-4 border-black rounded skew-x-6 grid grid-cols-3 gap-4 items-center"
+        >
+          <span class="font-komika">
+            {game.game_villian.card.name}
+          </span>
+
+          <span class="text-sm">{Calendar.strftime(game.inserted_at, "%a %b %d %H:%M %p")}</span>
+
+          <div class="flex flex-row justify-end gap-2">
+            <button class="btn btn-ghost btn-circle">
+              <.icon name="hero-trash" class="size-6 text-red-600" />
+            </button>
+            <button class="btn btn-ghost btn-circle">
+              <.link navigate={~p"/games/#{game.id}"}>
+                <.icon name="hero-arrow-right-on-rectangle" class="size-6 text-green-600" />
+              </.link>
+            </button>
+          </div>
+        </div>
+      </div>
     </Layouts.app>
     """
   end
